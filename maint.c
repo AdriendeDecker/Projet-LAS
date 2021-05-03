00aa8921f6ad4f99b3197d2a14edbaa44fb7d786
Adrien de Decker <20190116@IP-XLE017-12.vinci.be>
2021-05-03 17:20:40 +0200

    suite maint

--- maint.c
+++ maint.c
@@ -19,44 +19,47 @@ struct program {
 
 
 
-int main(int argc, char const *argv[]){
-    // Vérification inputs Type (et option)
-    if(argc <= 1){
-        printf("Il manque un type !\n");
-        return 0;
-    }
 
-    if(!strcmp(argv[1], "3")){
-        printf("Le type est 3\n");
-        if(argc <= 2){
-            printf("Il manque un parametre d'option\n");
-            return 0;
-        }
-    }
-    // Fin de vérifications inputs
+int main(int argc, char const **argv){
+    int sem_id, shm_id;
 
     // Sélection du bon type d'action a effectuer
-    if(strcmp(argv[1], "1") == 0){
+    if(argv[1][0] == "1") {
         printf("action 1 : création des ressources partagées\n");
 
         //Create semaphore
-        int sem_id = sem_create(SEMKEY, 1, PERM, 1);
-        printf("mon sem_id = %d\n",sem_id);
-        
+        sem_id = sem_create(SEMKEY, 1, PERM, 1);
+                
         //Create Shared Mermory
-        int shm_id = sshmget(SHMKEY, sizeof(struct program[1000]) + sizeof(int) , IPC_CREAT | PERM);
-        printf("mon shm_id = %d\n", shm_id);
+        shm_id = sshmget(SHMKEY, sizeof(struct program[1000]), IPC_CREAT | PERM);
+
+        printf("La création du répertoire de fichier partagé s'est effectué avec succès");
     } 
-    else if(strcmp(argv[1], "2") == 0){
+    else if(argv[1][0] == 2) {
         printf("action 2 : destruction des ressources partagées\n");
+        sem_id = sem_create(SEMKEY, 1, PERM, 1);
+        shm_id = sshmget(SHMKEY, sizeof(struct program[1000]), IPC_CREAT | PERM);
+        sshmdelete(shm_id);
+        sem_delete(sem_id);
+        printf("La suppression du répertoire de fichier partagé s'est effectué avec succès");
     }
-    else if(strcmp(argv[1], "3") == 0){
+    else if(argv[1][0] == 3){
         printf("action 3 : réservation exclusive des ressources partagées\n");
+        
+        // get attached memory id
+        sem_id = sem_create(SEMKEY, 1, PERM, 1);
+        shm_id = sshmget(SHMKEY, sizeof(struct program[1000]) + sizeof(int) , IPC_CREAT | PERM);
+
+        // take access to shared memory
+        sem_down0(sem_id);
+        // wait for delai of blocking
+        sleep(atoi(argv[2]));
+        // free the access to the shared memory
+        sem_up0(sem_id);
     }
     else { // Default action
         printf("Le type d'action que vous avez inséré est invalide\n");
         return 0;
     }
-
     return 0;
 }
