a3b6178342eaae036dd04887e0fb3b2328dd828c
AlbanBekteshi <albanbekteshi97@gmail.com>
2021-05-09 19:40:05 +0200

    addorchange

--- server.c
+++ server.c
@@ -42,6 +42,18 @@ int idx = -1;
 int size = -1;
 char buffer[BUFFER];
 
+static void exec_ls(void *arg){
+	char *scriptName = arg;
+	printf("-ls -l %s:\n", scriptName);
+	sexecl("/bin/ls","ls","-ls",scriptName,NULL);
+}
+
+static void exec_cat(void *arg){
+	char *scriptname = arg;
+	printf("cat %s : \n",scriptname);
+	sexecl("/bin/cat","cat",scriptname,NULL);
+}
+
 void readBlock(){
 	size = sread(0,buffer,BUFFER);	
 }
@@ -53,15 +65,35 @@ void addOrChange (int nbrCar,char* nomFichier, char* contenu){
 
 	fd = sopen(nomFichier, O_WRONLY | O_TRUNC | O_CREAT, 0644);
 
-	swrite(fd,contenu,nbrCar);
-	
+	/* Faire un ls -l sur le nom du fichier créé*/
+	int c1 = fork_and_run1(exec_ls,nomFichier);
+	swaitpid(c1,NULL,0);
 
+	/*Ecriture du shebang*/
+	char* shebang = "!/bin/bash\n";
+	int szShebang = strlen(shebang);
+	nwrite(fd,shebang,szShebang);
+
+	/*ecriture du contenu  */
+	nwrite(fd,contenu,nbrCar);
+
+	/*Liberation ressource*/
+	sclose(fd);
+
+	int c2 = fork_and_run1(exec_cat,nomFichier);
+	swaitpid(c2,NULL,0);
+
+	/*Execution*/
+	printf("./%s:\n",nomFichier);
+	sexecl(nomFichier,nomFichier,NULL);
 }
 
 int main(int argc, char *argv[])
 {
+
 	readBlock();
 	addOrChange(size,argv[1],buffer);
+	//sexecl("/bin/echo",argv[1]);
 	/* 
 	int sockfd;
 
