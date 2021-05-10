bd6dd008fe167a0d097dbe59bfbf706bd6bfc91e
bastienboutte <bastien.boutte@student.vinci.be>
2021-05-10 16:35:27 +0200

    add - part of the client program

--- client.c
+++ client.c
@@ -1,73 +1,134 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h>
 #include "utils_v10.h"
 
 #define COMMAND_SIZE 255
+#define MAX_PROGRAMMES 100
 
-/*void minuteurProcess(void *arg1, void *arg2) {
-    int *delay = arg1;
+void applyStrtoken (char* buffer, char** save);
+void add(char* adr, int port, char* path);
+void replace(char* adr, int port, int num, char* path);
+void execute1(char* adr, int port, int num);
+
+
+void minuteurProcess(void *arg1, void *arg2) {
+    int *delay = (int)arg1;
     int *pipefd = arg2;
+    int battement = -1;
 
-    sclose(pipefd[0]);
     //pipe écriture
+    sclose(pipefd[0]);
 
     //condition à trouver (via un quit ou autre par exemple)
     while(1) {
-        sleep(*delay);
+        sleep(delay);
+        swrite(pipefd[1],&battement, sizeof(int));
     }
 
     sclose(pipefd[1]);
 }
 
-void execProcess(void *arg) {
-    int *pipefd = arg;
+void execProcess(void *arg1, void *arg2, void *arg3) {
+    int port = (int)arg1;
+    char *adr = arg2;
+    int *pipefd = arg3;
+    int nbrProgrammes = 0;
+    int tabProgrammes[MAX_PROGRAMMES];
+    int numProgramme;
 
-    sclose(pipefd[1]);
     //pipe lecture
+    sclose(pipefd[1]);
+
+    int nbInt = read(pipefd[0], &numProgramme, sizeof(int));
+    while(nbInt > 0) {
+        tabProgrammes[nbrProgrammes] = numProgramme;
+        nbrProgrammes++;
+    }
 
     sclose(pipefd[0]);
-}*/
+}
 
 int main(int argc, char** argv) {
-    /*int pipefd[2];
-    int delay = argv[3];
+
+    int pipefd[2];
+    char bufferCmd[COMMAND_SIZE];
+    char* save[3];
+    bool validCmd = false;
+    bool stop = false;
+
+    int port = atoi(argv[2]);
+    int delay = atoi(argv[3]);
+    char* adr = atoi(argv[1]);
 
     spipe(pipefd);
-    int childMinuteur = fork_and_run2(minuteurProcess, delay, pipefd);
-    int childExec = fork_and_run1(execProcess, pipefd);
-    sclose(pipefd[0]);
+    int childExec = fork_and_run3(execProcess, &port, &adr, pipefd);
+    int childMinuteur = fork_and_run2(minuteurProcess, &delay, pipefd);
     //pipe écriture
-
-    //mettre un wait dans une boucle (max 100 ou jusqu'au bout des programmes) qui attend le minuteur avant d'exécuter la commande suivante ?
-    int i = 0;
-    while(tous programmes pas finis || i < 100) {
-       
-    }
-
-    sclose(pipefd[1]);*/
-
-    char bufferCmd[COMMAND_SIZE];
-    bool validCmd = false;
-
-    printf("Veuillez introduire votre commande (+,.,*,@) suivie de la donnée requise : \n");
-    int nbChar = read(0, bufferCmd, COMMAND_SIZE);
-    while(!validCmd){
-        if(bufferCmd[0] == "+") {
-            char bufferAdd[BUFFER_SIZE];
-            validCmd = true;
-        } else if(bufferCmd[0] == ".") {
-            validCmd = true;
-        } else if(bufferCmd[0] == "@") {
-            validCmd = true;
-        } else if(bufferCmd[0] == "*") {
-            validCmd = true;
-        } else if(bufferCmd[0] == "q") {
-            //free la liste des programmes (via un signal pour le minuteur et via la fermeture du pipe pour les programmes récurrents)
-            exit(0);
-        } else {
-            printf("Commande invalide, veuillez réintroduire votre commande (+,.,*,@) suivie de la donnée requise : \n");
-            nbChar = read(0, bufferCmd, COMMAND_SIZE);
+    sclose(pipefd[0]);
+    
+    while(!stop) {
+        printf("Veuillez introduire votre commande (+,.,*,@) suivie de la donnée requise : \n");
+        applyStrtoken(bufferCmd, save);
+        char* path;
+        int num;
+        while(!validCmd){
+            if(save[0] == "+") {
+                path = save[1];
+                add(adr, port, path);
+                validCmd = true;
+            } else if(save[0] == ".") {
+                num = atoi(save[1]);
+                path = save[2];
+                replace(adr, port, num, path);
+                validCmd = true;
+            } else if(save[0] == "@") {
+                num = atoi(save[1]);
+                execute1(adr, port, num);
+                validCmd = true;
+            } else if(save[0] == "*") {
+                num = atoi(save[1]);
+                //voir avec fils
+                validCmd = true;
+            } else if(save[0] == "q") {
+                stop = true;
+                //free la liste des programmes (via un signal pour le minuteur et via la fermeture du pipe pour les programmes récurrents)
+            } else {
+                printf("Commande invalide, veuillez réintroduire votre commande (+,.,*,@) suivie de la donnée requise : \n");
+                applyStrtoken(bufferCmd, save);
+            }
+        }
+        for(int i = 0; i<3; i++) {
+            free(save[i]);
         }
     }
+
+    sclose(pipefd[1]);
+
+    exit(0);
+}
+
+void add(char* adr, int port, char* path) {
+
+}
+
+void replace(char* adr, int port, int num, char* path) {
+
+}
+
+void execute1(char* adr, int port, int num) {
+
+}
+
+void applyStrtoken (char* buffer, char** save) {
+    sread(0, buffer, COMMAND_SIZE);
+    char* separator = " ";
+    char* strToken = strtok(buffer, separator);
+    int i = 0;
+    while (strToken != NULL) {
+        save[i] = strToken;
+        i++;
+        strToken = strtok(NULL, separator);
+    }   
 }
